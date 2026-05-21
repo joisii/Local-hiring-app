e6da3a684266c93e48c84662be1628dac0652392
merndeveloper <itsmejoel222@gmail.com>
2026-05-21 19:05:49 +0530

    enhanced the ui of login and register page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
- import App from './App.jsx'
 
-import { AuthProvider } from './context/AuthContext.jsx'
+import App from "./App.jsx";
+
+import { ConfigProvider } from "antd";
+
+import "antd/dist/reset.css";
+
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#1677ff",
+          borderRadius: 10,
+          fontFamily: "Inter, sans-serif"
+        }
+      }}
+    >
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ConfigProvider>
   </React.StrictMode>
 );
