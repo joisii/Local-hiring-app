5fe913505b06e1a4843f8d3420f17943ace8bd3b
merndeveloper <itsmejoel222@gmail.com>
2026-05-29 21:33:23 +0530

    Updated frontend API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://local-hiring-app.onrender.com/",
 
   withCredentials: true
 });
